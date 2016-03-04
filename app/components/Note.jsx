fb21ce4cdd95dd7d2fbd0d3dc195b2d1f4698a05
Yuri Zhou <t_zhouy@ads.autodesk.com>
2016-03-04 15:44:44 +0800

    implement basic kanban app

--- app/components/Note.jsx
+++ app/components/Note.jsx
@@ -1,68 +1,65 @@
 import React from 'react';
 
 export default class Note extends React.Component {
-	constructor(props) {
-		super(props);
+  constructor(props) {
+    super(props);
 
-		this.state = {
-			editing: false
-		};
-	}
+    this.state = {
+      editing: false
+    };
+  }
+  render() {
+    if(this.state.editing) {
+      return this.renderEdit();
+    }
 
-	render() {
-		if (this.state.editing) {
-			return this.renderEdit();
-		} else {
-			return this.renderNote();
-		}
-	}
+    return this.renderNote();
+  }
+  renderEdit = () => {
+    return <input type="text"
+      ref={
+        (e) => e ? e.selectionStart = this.props.task.length : null
+      }
+      autoFocus={true}
+      defaultValue={this.props.task}
+      onBlur={this.finishEdit}
+      onKeyPress={this.checkEnter} />;
+  };
+  renderDelete = () => {
+    return <button
+      className="delete-note"
+      onClick={this.props.onDelete}>x</button>;
+  };
+  renderNote = () => {
+    const onDelete = this.props.onDelete;
 
-	renderEdit = () => {
-		return <input type="text" 
-			ref={
-				(e) => e? e.selectionStart = this.props.task.length : null
-			}
-			autoFocus={true}
-			defaultValue={this.props.task}
-			onBlur={this.finishEdit}
-			onKeyPress={this.checkEnter} />;
-	};
+    return (
+      <div onClick={this.edit}>
+        <span className="task">{this.props.task}</span>
+        {onDelete ? this.renderDelete() : null }
+      </div>
+    );
+  };
+  edit = () => {
+    this.setState({
+      editing: true
+    });
+  };
+  checkEnter = (e) => {
+    if(e.key === 'Enter') {
+      this.finishEdit(e);
+    }
+  };
+  finishEdit = (e) => {
+    const value = e.target.value;
 
-	renderNote = () => {
-		const onDelete = this.props.onDelete;
+    if(this.props.onEdit) {
+      this.props.onEdit(value);
 
-		return (
-			<div onClick={this.edit}>
-				<span className="task">{this.props.task}</span>
-				{onDelete ? this.renderDelete() : null}
-			</div>);
-	};
-
-	renderDelete = () => {
-		return <button className="delete-note" onClick={this.props.onDelete}>x</button>;
-	};
-
-	edit = () => {
-		this.setState({
-			editing: true
-		});
-	};
-	checkEnter = (e) => {
-		if (e.key === 'Enter') {
-			this.finishEdit(e);
-		}
-	};
-	finishEdit = (e) => {
-		const value = e.target.value;
-
-		if (this.props.onEdit) {
-			this.props.onEdit(value);
-
-			this.setState({
-				editing: false
-			});
-		}
-	};
-
-
+      // Exit edit mode.
+      this.setState({
+        editing: false
+      });
+    }
+  };
 }
