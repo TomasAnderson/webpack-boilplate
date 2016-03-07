40cc77ab6cd8448df78b72f021dc4461e655293a
Yuri Zhou <t_zhouy@ads.autodesk.com>
2016-03-07 11:29:49 +0800

    implement basic drag and drop functions

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,26 +1,35 @@
-import AltContainer from 'alt-container';
 import React from 'react';
+import AltContainer from 'alt-container';
 import Lanes from './Lanes.jsx';
-import LaneActions from '../actions/LaneActions';
+import laneActions from '../actions/LaneActions';
 import LaneStore from '../stores/LaneStore';
+import {DragDropContext} from 'react-dnd';
+import HTML5Backend from 'react-dnd-html5-backend';
 
+@DragDropContext(HTML5Backend)
 export default class App extends React.Component {
-  render() {
-    return (
-      <div>
-        <button className="add-lane" onClick={this.addLane}>+</button>
-        <AltContainer
-          stores={[LaneStore]}
-          inject={{
-            lanes: () => LaneStore.getState().lanes || []
-          }}
-        >
-          <Lanes />
-        </AltContainer>
-      </div>
-    );
-  }
-  addLane() {
-    LaneActions.create({name: 'New lane'});
-  }
+
+	render() {
+
+		return (
+			<div>
+				<button className="add-Lane" onClick={this.addLane}>+</button>
+				<AltContainer 
+					stores={[LaneStore]}
+					inject={{
+						lanes: () => LaneStore.getState().lanes || []
+					}}
+				>
+					<Lanes />
+				</AltContainer>
+			</div>
+		);
+	}
+
+	addLane() {
+		laneActions.create({name: 'new lane'});
+	}
+
 }
+
+
